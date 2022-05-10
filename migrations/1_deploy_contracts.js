2aaa5bab479b3a505060debfd788c599172e135a
Pire Yohan <74233754+Pireyohan@users.noreply.github.com>
2022-05-10 23:13:34 +0200

    Delete 1_deploy_contracts.js

--- migrations/1_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-// Import du smart contract "SimpleStorage"
-const Voting = artifacts.require("Voting")
-
-module.exports = (deployer) => {
-  // Deployer le smart contract!
-  deployer.deploy(Voting)
-}
